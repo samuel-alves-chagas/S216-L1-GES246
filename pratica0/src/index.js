d078864f82b6223f30ae499be1ab04c3ddb4fa01
Samuel Alves Chagas <samuelchagasalves@gmail.com>
2024-02-26 21:00:37 -0300

    [entrega] - pratica 0

---
+++ pratica0/src/index.js
@@ -0,0 +1,9 @@
+// Exemplo de uso prático
+let nome = "Samuel - GES246";
+function saudacaoPersonalizada(nome) {
+    console.log("Olá, " + nome + "!");
+}
+saudacaoPersonalizada(nome);
+
+let modulo = require('./module.js');
+modulo.saudacao();
---
+++ pratica0/src/module.js
@@ -0,0 +1,7 @@
+// arquivo modulo.js
+module.exports = {
+  mensagem: 'Testando modulo: Samuel - GES246 !',
+  saudacao: function() {
+    console.log(this.mensagem);
+  }
+};
