b66b28775fb0c7999b21d3062556e2e9646ede4e
Samuel Alves Chagas <samuelchagasalves@gmail.com>
2024-05-14 22:38:13 -0300

    pratica 5

--- frontend/app.py
+++ frontend/app.py
@@ -5,7 +5,7 @@ import os
 app = Flask(__name__)
 
 # Definindo as variáveis de ambiente
-API_BASE_URL = os.getenv("API_BASE_URL" , "http://localhost:5000/api/v1/aluno")
+API_BASE_URL = os.getenv("API_BASE_URL" , "http://localhost:5000/api/v1/professor")
 API_DATABASE_RESET = os.getenv("API_DATABASE_RESET" , "http://localhost:5000/api/v1/database/reset") 
 
 # Rota para a página inicial
@@ -15,80 +15,80 @@ def index():
 
 # Rota para exibir o formulário de cadastro
 @app.route('/inserir', methods=['GET'])
-def inserir_aluno_form():
+def inserir_professor_form():
     return render_template('inserir.html')
 
 # Rota para enviar os dados do formulário de cadastro para a API
 @app.route('/inserir', methods=['POST'])
-def inserir_aluno():
+def inserir_professor():
     nome = request.form['nome']
-    curso = request.form['curso']
-    data_nascimento = request.form['data_nascimento']
+    disciplina = request.form['disciplina']
+    email = request.form['email']
 
     payload = {
         'nome': nome,
-        'curso': curso,
-        'dataNascimento': data_nascimento
+        'disciplina': disciplina,
+        'email': email
     }
 
     response = requests.post(f'{API_BASE_URL}/inserir', json=payload)
     
     if response.status_code == 201:
-        return redirect(url_for('listar_alunos'))
+        return redirect(url_for('listar_professors'))
     else:
-        return "Erro ao inserir aluno", 500
+        return "Erro ao inserir professor", 500
 
-# Rota para listar todos os alunos
+# Rota para listar todos os professores
 @app.route('/listar', methods=['GET'])
-def listar_alunos():
+def listar_professors():
     response = requests.get(f'{API_BASE_URL}/listar')
-    alunos = response.json()
-    return render_template('listar.html', alunos=alunos)
+    professores = response.json()
+    return render_template('listar.html', professores=professores)
 
-# Rota para exibir o formulário de edição de aluno
-@app.route('/atualizar/<int:aluno_id>', methods=['GET'])
-def atualizar_aluno_form(aluno_id):
+# Rota para exibir o formulário de edição de professor
+@app.route('/atualizar/<int:professor_id>', methods=['GET'])
+def atualizar_professor_form(professor_id):
     response = requests.get(f"{API_BASE_URL}/listar")
-    #filtrando apenas o aluno correspondente ao ID
-    alunos = [aluno for aluno in response.json() if aluno['id'] == aluno_id]
-    if len(alunos) == 0:
-        return "Aluno não encontrado", 404
-    aluno = alunos[0]
-    return render_template('atualizar.html', aluno=aluno)
+    #filtrando apenas o professor correspondente ao ID
+    professores = [professor for professor in response.json() if professor['id'] == professor_id]
+    if len(professores) == 0:
+        return "Professor não encontrado", 404
+    professor = professores[0]
+    return render_template('atualizar.html', professor=professor)
 
-# Rota para enviar os dados do formulário de edição de aluno para a API
-@app.route('/atualizar/<int:aluno_id>', methods=['POST'])
-def atualizar_aluno(aluno_id):
+# Rota para enviar os dados do formulário de edição de professor para a API
+@app.route('/atualizar/<int:professor_id>', methods=['POST'])
+def atualizar_professor(professor_id):
     nome = request.form['nome']
-    curso = request.form['curso']
-    data_nascimento = request.form['data_nascimento']
+    disciplina = request.form['disciplina']
+    email = request.form['email']
 
     payload = {
-        'id': aluno_id,
+        'id': professor_id,
         'nome': nome,
-        'curso': curso,
-        'dataNascimento': data_nascimento
+        'disciplina': disciplina,
+        'email': email
     }
 
     response = requests.post(f"{API_BASE_URL}/atualizar", json=payload)
     
     if response.status_code == 200:
-        return redirect(url_for('listar_alunos'))
+        return redirect(url_for('listar_professors'))
     else:
-        return "Erro ao atualizar aluno", 500
+        return "Erro ao atualizar professor", 500
 
-# Rota para excluir um aluno
-@app.route('/excluir/<int:aluno_id>', methods=['POST'])
-def excluir_aluno(aluno_id):
-    #payload = {'id': aluno_id}
-    payload = {'id': aluno_id}
+# Rota para excluir um professor
+@app.route('/excluir/<int:professor_id>', methods=['POST'])
+def excluir_professor(professor_id):
+    #payload = {'id': professor_id}
+    payload = {'id': professor_id}
 
     response = requests.post(f"{API_BASE_URL}/excluir", json=payload)
     
     if response.status_code == 200  :
-        return redirect(url_for('listar_alunos'))
+        return redirect(url_for('listar_professors'))
     else:
-        return "Erro ao excluir aluno", 500
+        return "Erro ao excluir professor", 500
 
 #Rota para resetar o database
 @app.route('/reset-database', methods=['GET'])
